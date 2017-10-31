3e2be419b89eb1629802c69ba70d166c9278436d
David A Roberts <d@vidr.cc>
2017-10-31 14:41:27 +1000

    benchmark: measerr, birats

--- stochaskell/app/Benchmark.hs
+++ stochaskell/app/Benchmark.hs
@@ -4,8 +4,8 @@
 module Main where
 import Language.Stochaskell
 
-import qualified Data.Expression as E
 import Data.Expression.Eval
+import Data.List (transpose)
 import Data.Maybe
 import Data.Time.Clock.POSIX
 
@@ -82,13 +82,47 @@ gpclas n = do
   (g,phi) <- gpClassifier (kernelSE' lsv lls2) n x
   return (lsv,lls2,x,g,phi)
 
+measerr :: Z -> R -> P (R,R,RVec,RVec,R,R,R,RVec)
+measerr n tau = do
+  xMu <- uniform (-10) 10
+  xSigma <- uniform 0 10
+  x <- normals (vector [ xMu | i <- 1...n ])
+               (vector [ xSigma | i <- 1...n ])
+  xMeas <- normals x (vector [ tau | i <- 1...n ])
+  alpha <- normal 0 10
+  beta <- normal 0 10
+  sigma <- truncated 0 infinity (cauchy 0 5)
+  y <- normals (bsxfun (+) alpha (beta *> x)) (vector [ sigma | i <- 1...n ])
+  return (xMu,xSigma,x,xMeas,alpha,beta,sigma,y)
+
+birats :: Z -> Z -> P (RVec,RVec,RMat,RMat,R,RMat)
+birats n t = do
+  x <- uniforms (vector [  0 | j <- 1...t ])
+                (vector [ 50 | j <- 1...t ])
+  betaMu <- normals (vector [   0 | _ <- 1...2 ])
+                    (vector [ 100 | _ <- 1...2 ])
+  tau <- joint vector [ truncated 0 infinity (cauchy 0 2.5) | i <- 1...2 ]
+  corr <- corrLKJ 2 (1,2)
+  -- TODO: automatically recognise common forms
+  --let betaSigma = diag tau <> corr <> diag tau
+  let betaSigma = qfDiag corr tau
+  beta <- normalsChol n 2 betaMu betaSigma
+  let beta' = tr' beta
+      beta1 = beta'!1
+      beta2 = beta'!2
+  --let yMu = matrix [ (beta1!i) + (beta2!i) * (x!j) | i <- 1...n, j <- 1...t ]
+  let yMu = bsxfun (+) (asColumn beta1) (outer beta2 x)
+  yVar <- gamma 1 1
+  y <- normals yMu (matrix [ sqrt yVar | i <- 1...n, j <- 1...t ])
+  return (x,betaMu,betaSigma,beta,yVar,y)
+
 covtype :: IO ()
 covtype = do
   let n = 581012; d = 54
   --(xData,wTrue,bTrue,yData) <- simulate (model n d)
   table <- readRealMatrix "data/covtype.std.data" (1,n) (1,55)
-  let xData = E.const $ table `slice` [[i,j] | i <- 1...n, j <- 1...d]
-      yData = E.const . binarize (1 ==) $ table `slice` [[i,55] | i <- 1...n]
+  let xData = constExpr $ table `slice` [[i,j] | i <- 1...n, j <- 1...d]
+      yData = constExpr . binarize (1 ==) $ table `slice` [[i,55] | i <- 1...n]
 
   let post = [ (w,b) | (x,w,b,y) <- logreg n d, x == xData, y == yData ]
       stepSize = 0.0001
@@ -109,7 +143,7 @@ poly' = do
   (xData,alphaTrue,betaTrue,yData) <- simulate (poly n d)
   let design = matrix [ let p = cast (j-1) in (xData!i)**p
                       | i <- 1...n, j <- 1...(d+1) ] :: RMat
-      design' = E.const . fromJust $ eval_ design
+      design' = constExpr . fromJust $ eval_ design
   let post = [ (a,b) | (x,a,b,y) <- nlm n (d+1), x == design', y == yData ]
       stepSize = 0.1
   (_,alphaInit,betaInit,_) <- simulate (poly n d)
@@ -138,6 +172,38 @@ gpc = do
   putStrLn msgPyMC3
   putStrLn msgEdward
 
+measerr' :: IO ()
+measerr' = do
+  let n = 1000; tau = 0.1
+  (xMuTrue,xSigmaTrue,xTrue,xMeasData,alphaTrue,betaTrue,sigmaTrue,yData) <- simulate (measerr n tau)
+  let post = [ (xm,xs,x,a,b,s) | (xm,xs,x,xx,a,b,s,y) <- measerr n tau, xx == xMeasData, y == yData ]
+  (xMuInit,xSigmaInit,xInit,_,alphaInit,betaInit,sigmaInit,_) <- simulate (measerr n tau)
+  let init = (xMuInit,xSigmaInit,xInit,alphaInit,betaInit,sigmaInit)
+  samplesStan <- hmcStanInit 1000 post init
+  putStrLn $ pmProgram post
+  samplesPyMC3 <- runPyMC3 defaultPyMC3Inference{pmDraws=1000,pmInit=Nothing,pmTune=1000} post (Just init)
+  putStrLn "==="
+  putStrLn $ "TRUTH:\t"++ show (xMuTrue,xSigmaTrue,xTrue,alphaTrue,betaTrue,sigmaTrue)
+  print $ last samplesStan
+  print $ last samplesPyMC3
+
+birats' :: IO ()
+birats' = do
+  let n = 100; t = 10
+  (xData,bmTrue,bsTrue,bTrue,yvTrue,yData) <- simulate (birats n t)
+  let post = [ (bm,bs,b,yv) | (x,bm,bs,b,yv,y) <- birats n t, x == xData, y == yData ]
+  (_,bmInit,bsInit,bInit,yvInit,_) <- simulate (birats n t)
+  let init = (bmInit,bsInit,bInit,yvInit)
+  samplesStan <- hmcStanInit 1000 post init
+  putStrLn $ pmProgram post
+  samplesPyMC3 <- runPyMC3 defaultPyMC3Inference{pmDraws=1000,pmInit=Nothing,pmTune=1000} post (Just init)
+  putStrLn "==="
+  putStrLn $ "TRUTH:\t"++ show (bmTrue,yvTrue)
+  let (bmSample,_,_,yvSample) = last samplesStan
+  print (bmSample,yvSample)
+  let (bmSample,_,_,yvSample) = last samplesPyMC3
+  print (bmSample,yvSample)
+
 benchStanHMC numSamp numSteps stepSize p init = do
   ticStan <- getPOSIXTime
   let method = defaultStanMethod
@@ -151,9 +217,8 @@ benchStanHMC numSamp numSteps stepSize p init = do
         }
   samples <- runStan method p init
   tocStan <- getPOSIXTime
-  let aStan = mean (map fst samples)
-      bStan = mean (map snd samples)
-  return $ "STAN:\t"++   show (aStan,   bStan)   ++" took "++ show (tocStan   - ticStan)
+  let means = map mean . transpose $ map (map (fromJust . evalD_) . fromExprTuple) samples
+  return $ "STAN:\t"++ show means ++" took "++ show (tocStan - ticStan)
 
 benchPyMC3HMC numSamp numSteps stepSize p init = do
   putStrLn $ pmProgram p
@@ -183,4 +248,4 @@ benchEdwardHMC numSamp numSteps stepSize p init = do
       bEdward = mean (map snd samples)
   return $ "EDWARD:\t"++ show (aEdward, bEdward) ++" took "++ show (tocEdward - ticEdward)
 
-main = gpc
+main = birats'
