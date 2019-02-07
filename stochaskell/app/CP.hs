e28d898850b132dae23b94631aa6e4c984576f10
David A Roberts <d@vidr.cc>
2019-02-07 16:08:24 +1000

    changepoint stan

--- stochaskell/app/CP.hs
+++ stochaskell/app/CP.hs
@@ -13,26 +13,39 @@ noise = 1e-6
 kernelSE' eta ils x y =
   eta * exp (- ((x - y) * ils)^2 / 2)
 
+prior :: R -> Z -> P (R,R,R,R,RVec,R)
+prior t n = do
+  eta1 <- gamma 1 1
+  eta2 <- gamma 1 1
+  ils1 <- gamma 1 1
+  ils2 <- gamma 1 1
+  s <- orderedSample n (uniform 0 t)
+  c <- uniform 0 t
+  return (eta1,ils1,eta2,ils2,s,c)
+
 gpChangepoint :: (R -> R -> R) -> (R -> R -> R) -> (Z,RVec,R) -> P RVec
 gpChangepoint kernel1 kernel2 (n,s,c) = do
-  let k = findSortedInsertIndex c s - 1
-      mu  = vector [ 0 | _ <- 1...n ]
-      cov1 = matrix [ kernel1 (s!i) (s!j)
-                      + if i == j then noise else 0
-                    | i <- 1...k, j <- 1...k ]
-      cov2 = matrix [ kernel2 (s!(k+i)) (s!(k+j))
-                      + if i == j then noise else 0
-                    | i <- 1...(n-k), j <- 1...(n-k) ]
-      cov = blockMatrix [[cov1, zeros k (n-k)]
-                        ,[zeros (n-k) k, cov2]]
-  g <- normalChol n mu cov
+  let mu  = vector [ 0 | _ <- 1...n ]
+      cov = matrix [ let a = s!i; b = s!j in 
+          (if i == j then noise else 0) +
+          if a < c
+          then if b < c then kernel1 a b else 0
+          else if b < c then 0 else kernel2 a b
+        | i <- 1...n, j <- 1...n ]
+  g <- normal mu cov
   return g
 
 main = do
   let n = 100
       s = list $ (0.1 *) <$> [1..100]
-      c = 5
-  g <- sampleP $ gpChangepoint (kernelSE' 1 1) (kernelSE' 0.2 0.2) (n,s,c)
-  print g
+  (eta1,ils1,eta2,ils2,_,c) <- sampleP (prior 10 n)
+  let k1 = kernelSE' eta1 ils1
+      k2 = kernelSE' eta2 ils2
+  g <- sampleP $ gpChangepoint k1 k2 (n,s,c)
   toFile def "cp_data.png" $ do
     plot $ line "data" [sort $ zip (s :: [Double]) (list g :: [Double])]
+  samples <- hmcStanInit 1000 [c' | (eta1',ils1',eta2',ils2',s',c') <- prior 10 n
+                                  , g' <- gpChangepoint k1 k2 (n,s',c')
+                                  , s' == s, g' == g] 5
+  print c
+  print (last samples)
